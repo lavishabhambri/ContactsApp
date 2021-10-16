6d10b0fdda41172f42c2f7191b3dbe84ea436f53
Lavisha Bhambri <bhambrilavisha@gmail.com>
2021-10-17 01:19:57 +0530

    Code refractored

--- routes/auth.js
+++ routes/auth.js
@@ -68,6 +68,7 @@ router.post('/signin',(req,res)=>{
         bcrypt.compare(password,savedUser.password)
         .then(doMatch=>{
             if(doMatch){
+                // res.json({message:"successfully signed in"})
                const token = jwt.sign({_id:savedUser._id},JWT_SECRET)
                const {_id,name,email,followers,following} = savedUser
                res.json({token,user:{_id,name,email,followers,following}})
